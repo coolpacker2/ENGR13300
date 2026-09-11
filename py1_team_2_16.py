881ab8161727ecfee4b9013321fb6f78e51ad729
coolpacker2 <stephenlimzy2008@gmail.com>
2026-09-11 13:30:50 -0400

    update

--- py1_team_2_16.py
+++ py1_team_2_16.py
@@ -3,7 +3,7 @@ Course Number: ENGR 13300
 Semester: Fall 2026
 
 Description:
-    Team task 2 part b program
+    Team task 2 part b program. We aim to test random seeds and sum numbers of different types.
 
 Assignment Information:
     Assignment:     13.2.2 Team task 2
@@ -37,29 +37,25 @@ Academic Integrity Statement:
 import random
 from fractions import Fraction
 
-# def main():
-#     """Write your code here (and delete this line)."""
+def main():
+    random.seed(int(input("Input the seed: ")))
 
+    var_1 = round(random.random() * 100,3)
+    var_2 = round(10 + random.random() * 40,3)
+    var_3 = round(20 + random.random() * 20,3)
+    var_4 = round(100 + random.random() * 100,3)
+    var_1_frac = Fraction(var_1).limit_denominator(1000)
+    var_2_frac = Fraction(var_2).limit_denominator(1000)
+    var_3_frac = Fraction(var_3).limit_denominator(1000)
+    var_4_frac = Fraction(var_4).limit_denominator(1000)
 
-# if __name__ == "__main__":
-#     main()
-
-random.seed(int(input("Input the seed: ")))
-
-var_1 = round(random.random() * 100,3)
-var_2 = round(10 + random.random() * 40,3)
-var_3 = round(20 + random.random() * 20,3)
-var_4 = round(100 + random.random() * 100,3)
-var_1_frac = Fraction(var_1).limit_denominator(1000)
-var_2_frac = Fraction(var_2).limit_denominator(1000)
-var_3_frac = Fraction(var_3).limit_denominator(1000)
-var_4_frac = Fraction(var_4).limit_denominator(1000)
-
-var_sum_float = var_1 + var_2 + var_3 + var_4
-var_sum_fractions = Fraction.limit_denominator(var_1_frac + var_2_frac + var_3_frac + var_4_frac, 1000)
-print(f"First Random Number : {var_1}")
-print(f"Second Random Number : {var_2}")
-print(f"Third Random Number : {var_3}")
-print(f"Fourth Random Number : {var_4}")
-print(f"Sum from decimals: {var_1} + {var_2} + {var_3} + {var_4} = {var_sum_float}")
-print(f"Sum from fractions: {var_1_frac} + {var_2_frac} + {var_3_frac} + {var_4_frac} = {var_sum_fractions}")
+    var_sum_float = var_1 + var_2 + var_3 + var_4
+    var_sum_fractions = Fraction.limit_denominator(var_1_frac + var_2_frac + var_3_frac + var_4_frac, 1000)
+    print(f"First Random Number : {var_1}")
+    print(f"Second Random Number : {var_2}")
+    print(f"Third Random Number : {var_3}")
+    print(f"Fourth Random Number : {var_4}")
+    print(f"Sum from decimals: {var_1} + {var_2} + {var_3} + {var_4} = {var_sum_float}")
+    print(f"Sum from fractions: {var_1_frac} + {var_2_frac} + {var_3_frac} + {var_4_frac} = {var_sum_fractions}")
+if __name__ == "__main__":
+     main()
