a8386a62305d3543978918b397b4af238732f569
coolpacker2 <stephenlimzy2008@gmail.com>
2026-09-11 13:41:50 -0400

    saving my changes

--- py1_team_2_16.py
+++ py1_team_2_16.py
@@ -6,7 +6,7 @@ Description:
     Team task 2 part b program. We aim to test random seeds and sum numbers of different types.
 
 Assignment Information:
-    Assignment:     13.2.2 Team task 2
+    Assignment:     13.2.2 py1 team 2
     Team ID:        LC5 - 16
     Author:         Zheng Yang Stephen Lim, lim573@purdue.edu
     Date:           09/11/2026
@@ -38,24 +38,30 @@ import random
 from fractions import Fraction
 
 def main():
-    random.seed(int(input("Input the seed: ")))
-
+    #initial input
+    random.seed(int(input("Enter the seed: ")))
+    #round all numbers to 3 decimal places
     var_1 = round(random.random() * 100,3)
     var_2 = round(10 + random.random() * 40,3)
     var_3 = round(20 + random.random() * 20,3)
     var_4 = round(100 + random.random() * 100,3)
+    #limit the denominator to 1000 for all fractions
     var_1_frac = Fraction(var_1).limit_denominator(1000)
     var_2_frac = Fraction(var_2).limit_denominator(1000)
     var_3_frac = Fraction(var_3).limit_denominator(1000)
     var_4_frac = Fraction(var_4).limit_denominator(1000)
-
+    #sum the numbers as floats
     var_sum_float = var_1 + var_2 + var_3 + var_4
+    #sum all the numbers as fractions
     var_sum_fractions = Fraction.limit_denominator(var_1_frac + var_2_frac + var_3_frac + var_4_frac, 1000)
+    #print each number
     print(f"First Random Number : {var_1}")
     print(f"Second Random Number : {var_2}")
     print(f"Third Random Number : {var_3}")
     print(f"Fourth Random Number : {var_4}")
+    #print decimal sum
     print(f"Sum from decimals: {var_1} + {var_2} + {var_3} + {var_4} = {var_sum_float}")
+    #print fraction sum
     print(f"Sum from fractions: {var_1_frac} + {var_2_frac} + {var_3_frac} + {var_4_frac} = {var_sum_fractions}")
 if __name__ == "__main__":
      main()
